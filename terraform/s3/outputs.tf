0d323b4852e2e5e415e7005add81b235e9754cbf
Joshua Hoblitt <josh@hoblitt.com>
2017-02-20 13:34:18 -0700

    add s3 bucket + role user creation

---
+++ terraform/s3/outputs.tf
@@ -0,0 +1,31 @@
+output "EUPS_S3_BUCKET" {
+  value = "${aws_s3_bucket.eups.id}"
+}
+
+output "EUPS_PUSH_USER" {
+  value = "${module.push-user.name}"
+}
+
+output "EUPS_PUSH_AWS_ACCESS_KEY_ID" {
+  sensitive = true
+  value     = "${module.push-user.id}"
+}
+
+output "EUPS_PUSH_AWS_SECRET_ACCESS_KEY" {
+  sensitive = true
+  value     = "${module.push-user.secret}"
+}
+
+output "EUPS_PULL_USER" {
+  value = "${module.pull-user.name}"
+}
+
+output "EUPS_PULL_AWS_ACCESS_KEY_ID" {
+  sensitive = true
+  value     = "${module.pull-user.id}"
+}
+
+output "EUPS_PULL_AWS_SECRET_ACCESS_KEY" {
+  sensitive = true
+  value     = "${module.pull-user.secret}"
+}
