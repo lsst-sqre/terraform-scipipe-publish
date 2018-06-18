73622db0de14bf605e6819cb7c25770919ef8742
Joshua Hoblitt <josh@hoblitt.com>
2018-06-18 12:05:12 -0700

    fix description of k8s_namespace variable

--- tf/modules/pkgroot/variables.tf
+++ tf/modules/pkgroot/variables.tf
@@ -33,7 +33,7 @@ variable "k8s_client_key" {}
 variable "k8s_cluster_ca_certificate" {}
 
 variable "k8s_namespace" {
-  description = "DNS domain name to use when creating route53 records."
+  description = "k8s namespace to manage resources within."
   default     = "pkgroot"
 }
 
