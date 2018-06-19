f5e85c3385de7b38022d6fa275505b38e257fc16
Joshua Hoblitt <josh@hoblitt.com>
2018-06-19 09:46:46 -0700

    mv route53 resource -> route53.tf

--- tf/modules/doxygen/main.tf
+++ tf/modules/doxygen/main.tf
@@ -106,13 +106,3 @@ resource "aws_s3_bucket_object" "doxygen_index" {
 </html>
 EOF
 }
-
-resource "aws_route53_record" "doxygen_www" {
-  zone_id = "${var.aws_zone_id}"
-
-  # remove "<env>-" prefix for production
-  name    = "${data.template_file.fqdn.rendered}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["${aws_s3_bucket.doxygen.website_endpoint}"]
-}
